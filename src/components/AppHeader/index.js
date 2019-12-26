8abf38fd1635bbb05d8a280a4499cb7ff725efdc
Ganesan Karuppaiya <ganesank@live.com>
2019-12-27 00:40:02 +0530

    menu update

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -13,7 +13,7 @@ import {
 const AppHeader = props => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-  const title = (listitem) => {
+  const title = listitem => {
     switch (listitem.type) {
       case "social":
         return "SOCIAL";
@@ -32,28 +32,40 @@ const AppHeader = props => {
     });
     return men;
   };
+  const getMenuList = replists => {
+    const men = [];
+    replists.forEach(function(item, index) {
+      men.push(<a  href={`#${item.type}`} >     {title(item)} </a>);
+    });
+    return men;
+  };
   const menuArr = getMenu(props.menu);
+  const menuArrList = getMenuList(props.menu);
   return (
-    <div>
+    <div><ScrollspyNav scrollTargetIds={menuArr} activeNavClass="is-active">
       <Navbar color="light" light expand="md" className="fixed-top">
         <NavbarBrand href="/">Ganesan Karuppaiya</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <ScrollspyNav scrollTargetIds={menuArr} activeNavClass="is-active">
-            <Nav className="mr-auto" navbar>
-              {props.menu &&
-                props.menu.length > 0 &&
-                props.menu.map((item, index) => {
-                  return (
-                    <NavItem>
-                      <NavLink href={`#/${item.type}`}> {title(item)}</NavLink>
-                    </NavItem>
-                  );
-                })}
-            </Nav>
-          </ScrollspyNav>
+          <Nav className="mr-auto" navbar>
+            {props.menu &&
+              props.menu.length > 0 &&
+              props.menu.map((item, index) => {
+                return (
+                  <NavItem>
+                    <NavLink href={`#${item.type}`}>
+                      {" "}
+                      {title(item)}
+                    </NavLink>
+                  </NavItem>
+                );
+              })}
+
+          </Nav>
         </Collapse>
       </Navbar>
+     
+      </ScrollspyNav>
     </div>
   );
 };
